0d002a5b53d6d0ed931e19391bbddf452920023b
Raveen <rmaldeni@gmail.com>
2023-06-29 23:20:56 +1000

    add eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,43 @@
-module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
-}
+export default [
+	{
+		env: { browser: true, es2020: true },
+		extends: [
+			"eslint:recommended",
+			"plugin:react/recommended",
+			"plugin:react-hooks/recommended",
+			"plugin:import/recommended",
+			"plugin:jsx-a11y/recommended",
+			"plugin:@typescript-eslint/recommended",
+			"eslint-config-prettier",
+			"plugin:import/typescript"
+		],
+		parser: "@typescript-eslint/parser",
+		parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+		plugins: ["react-refresh"],
+		settings: {
+			react: {
+				version: "detect"
+			},
+			"import/resolver": {
+				node: {
+					extensions: [".js", ".jsx", ".ts", ".tsx", ".svg"],
+					moduleDirectory: ["node_modules", "src/"]
+				}
+			}
+		},
+		rules: {
+			"react-refresh/only-export-components": "warn",
+			"@typescript-eslint/no-explicit-any": "error",
+			"no-unused-vars": [
+				"error",
+				{
+					vars: "all",
+					args: "after-used",
+					ignoreRestSiblings: true,
+					argsIgnorePattern: "^_"
+				}
+			],
+			"react/react-in-jsx-scope": "off"
+		}
+	}
+];
