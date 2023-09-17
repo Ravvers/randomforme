d39db93c4617b07e7f6946b1a74be02bb53bbf2c
Raveen Maldeni <rmaldeni@gmail.com>
2023-09-17 15:48:49 +1200

    add basic number generation

---
+++ src/pages/Randomise/Generate/Generate.tsx
@@ -0,0 +1,76 @@
+import { Button, TextField } from "@mui/material";
+import { PageContent } from "../../../components/PageContent/PageContent";
+import { ChangeEvent, useState } from "react";
+import { generateRandomIntegers } from "../../../randomise/randomise";
+
+export const Generate = () => {
+	function generateNumbersAndDisplay(): void {
+		const generatedNumbers: number[] = generateRandomIntegers({
+			quantity: quantity,
+			range: { min: minimum, max: maximum }
+		});
+		setGeneratedNumbers(generatedNumbers);
+	}
+
+	function handleInputChange(
+		e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+		name: string
+	) {
+		const target = e.target as HTMLInputElement;
+		const value = target.value;
+
+		if (name === "quantity") {
+			setQuantity(parseInt(value));
+		} else if (name === "minimum") {
+			setMinimum(parseInt(value));
+		} else if (name === "maximum") {
+			setMaximum(parseInt(value));
+		}
+	}
+
+	const [quantity, setQuantity] = useState<number>(1);
+	const [minimum, setMinimum] = useState<number>(1);
+	const [maximum, setMaximum] = useState<number>(100);
+
+	const [generatedNumbers, setGeneratedNumbers] = useState<number[]>([]);
+
+	return (
+		<PageContent>
+			<TextField
+				id="outlined-basic"
+				label="Quantity"
+				variant="outlined"
+				type="number"
+				value={quantity}
+				onChange={(e) => handleInputChange(e, "quantity")}
+			/>
+			<div style={{ marginBottom: 20 }} />
+			<TextField
+				id="outlined-basic"
+				label="Minimum (inclusive)"
+				variant="outlined"
+				type="number"
+				value={minimum}
+				onChange={(e) => handleInputChange(e, "minimum")}
+				style={{ marginRight: 20 }}
+			/>
+			<TextField
+				id="outlined-basic"
+				label="Maximum (inclusive)"
+				variant="outlined"
+				type="number"
+				value={maximum}
+				onChange={(e) => handleInputChange(e, "maximum")}
+			/>
+			<div style={{ marginBottom: 20 }} />
+			<Button
+				variant="contained"
+				onClick={() => generateNumbersAndDisplay()}
+			>
+				Generate
+			</Button>
+			<div style={{ marginBottom: 20 }} />
+			<div>{generatedNumbers.join(", ")}</div>
+		</PageContent>
+	);
+};
